ce807844d1248246a393eeaf0af9e2cce0a1a8ee
Md. Abdur Rakib <99346417+Raquibul-Islam@users.noreply.github.com>
2024-05-10 14:49:30 +0600

    Add files via upload

---
+++ Sazzad_portfoolio/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rakibul's Website</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jomhuria&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+
+    <section class="top-banner" id="home">
+        <div class="half">
+            <h1>Welcome to</h1>
+            <h1><span class="orange-color">Rakibul's</span> Website</h1>
+            <h2>Software Engineer</h2>
+            <p>Modern web applications often contain three or more tiers, and depending on the size of the team a developer works on, he or she may specialize in one or more of these tiers - or may take a more interdisciplinary role. A web developer is usually classified as a Front-end web development or a Back-End Web Developer. For example, in a two-person team, one developer may focus on the technologies sent to the client such as C#, HTML, JavaScript, CSS, ReactJs or AngularJS and on the server-side frameworks (such as Perl, Python, Ruby, PHP, Java, ASP, ASP.NET, Node.js) used to deliver content and scripts to the client. 
+            </p>
+            <a class="link-button" target="_blank" href="https://www.linkedin.com/in/rakibul-islam02001/">HIRE ME</a>
+        </div> 
+        <div class="half profile-picture">
+            <img src="rakibul.png" alt="">
+        </div>
+    </section>
+
+    <section class="dream-area" id="about">
+        <div class="half">
+            <img id="sky" src="so.jpg"> 
+        </div>
+        <div class="half">
+            <h1>Dream Big</h1>
+            <h3>Become a Web Developer</h3>
+            <p>Becoming a web developer is an exciting and rewarding career path. Here are some suggestions to help you get started:
+                <p>Learn the basics: Start by learning HTML, CSS, and JavaScript </p>
+                <p>Choose a specialization: There are various specialties within web development such as front-end, back-end, or full-stack development. </p>
+                <p>Build a portfolio: Create a portfolio website showcasing your projects, skills, and achievements.</p>
+                <a class="link-button" target="_blank" href="https://www.linkedin.com/in/rakibul-islam02001/">See Linkedin Profile</a>
+        </div>
+    </section>  
+
+    <section class="experience" id="experience">
+        <h1>Experience</h1>
+        <div id="full-stack" class="half single-experience">
+            <h2>Full Stack Web developer</h2>
+            <h3 class="orange-color">2024-Present </h3>
+            <p>I am the master of C#, ASP.Net, HTML, CSS and Javascript. I know everything needed to make a website function, efficient. I didn't stop with the web. I went beyond with most popular Javascript framework called Vue JS. I even know the deployment, server and security. I will give you 100% web solution.</p>
+        </div>
+
+        <div id="baby-web" class="half single-experience">
+            <h2>Noob Web Developer</h2>
+            <h3 class="orange-color">2020-2021 | Programming Hero Learner</h3>
+            <p>They didn't offer me a job. But I made myself as a remove web developer. I made their website and showed it to them. They liked it. And uploaded the content. It was fun working at Programming Hero.
+            </p>
+        </div>
+
+        </section>
+
+
+
+ 
+
+    <div class="contact-info" id="contact">
+        <h2>Contact Us</h2>
+        <ul>
+            <li><i class="fa fa-map-marker"></i>Barisal, Bangladesh</li>
+            <li><i class="fa fa-phone"></i>+8801521549873</li>
+            <li><i class="fa fa-envelope"></i>sazzad17@cse.pstu.ac.bd</li>
+        </ul>
+    </div>
+
+    <footer>
+        <p>© Rakibul Islam 2024, all right reserved.</p>
+    </footer>
+</body>
+</html>
